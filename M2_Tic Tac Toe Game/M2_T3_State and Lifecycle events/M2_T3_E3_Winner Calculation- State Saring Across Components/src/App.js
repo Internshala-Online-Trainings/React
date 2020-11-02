73b208d9af4e227d50c2f9e3627910dc68515731
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-11-02 10:55:15 +0530

    Add files via upload

---
+++ M2_Tic Tac Toe Game/M2_T3_State and Lifecycle events/M2_T3_E3_Winner Calculation- State Saring Across Components/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Board from './components/Board';
+import { calculateWinner } from './helpers';
+
+import './styles/root.scss';
+
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
+
+export default App;
