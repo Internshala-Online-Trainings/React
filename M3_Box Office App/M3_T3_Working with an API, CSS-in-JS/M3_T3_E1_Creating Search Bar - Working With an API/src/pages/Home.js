b473536a7c85c49257b8687942e3e0adddfbf85a
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 15:01:37 +0530

    Add files via upload

---
+++ M3_Box Office App/M3_T3_Working with an API, CSS-in-JS/M3_T3_E1_Creating Search Bar - Working With an API/src/pages/Home.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+
+const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
