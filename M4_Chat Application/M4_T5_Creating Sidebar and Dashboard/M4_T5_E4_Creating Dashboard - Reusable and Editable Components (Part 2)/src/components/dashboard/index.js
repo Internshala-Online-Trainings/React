fbfce03f766a1d3a5ec548c203808dfa2402aebe
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:44:03 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T5_Creating Sidebar and Dashboard/M4_T5_E4_Creating Dashboard - Reusable and Editable Components (Part 2)/src/components/dashboard/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Drawer, Button, Divider } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
+
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  const onSave = async newData => {
+    console.log(newData);
+  };
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
+};
+
+export default Dashboard;
