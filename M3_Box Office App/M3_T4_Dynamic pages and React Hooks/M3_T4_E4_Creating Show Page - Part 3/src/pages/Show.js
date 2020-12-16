3c06b85b11b4dbbebf90100f8243c3c3f47c8d4a
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:54:13 +0530

    Add files via upload

---
+++ M3_Box Office App/M3_T4_Dynamic pages and React Hooks/M3_T4_E4_Creating Show Page - Part 3/src/pages/Show.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+
+  console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  return <div>this is show page</div>;
+};
+
+export default Show;
