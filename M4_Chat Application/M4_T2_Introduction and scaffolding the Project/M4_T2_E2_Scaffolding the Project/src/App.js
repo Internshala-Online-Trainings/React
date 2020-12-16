154ad59f88d5b343295a925c7dbc68b39c7a0340
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:21:23 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T2_Introduction and scaffolding the Project/M4_T2_E2_Scaffolding the Project/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+
+function App() {
+  return <div>hello</div>;
+}
+
+export default App;
