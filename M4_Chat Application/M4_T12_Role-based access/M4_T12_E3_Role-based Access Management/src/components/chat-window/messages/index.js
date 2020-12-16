e7e16d00b88bb9c0af5eba0ccdf274854feee4e4
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:38:00 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T12_Role-based access/M4_T12_E3_Role-based Access Management/src/components/chat-window/messages/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router';
+import { Alert } from 'rsuite';
+import { database } from '../../../misc/firebase';
+import { transformToArrWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
+
+const Messages = () => {
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messagesRef = database.ref('/messages');
+
+    messagesRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrWithId(snap.val());
+
+        setMessages(data);
+      });
+
+    return () => {
+      messagesRef.off('value');
+    };
+  }, [chatId]);
+
+  const handleAdmin = useCallback(
+    async uid => {
+      const adminsRef = database.ref(`/rooms/${chatId}/admins`);
+
+      let alertMsg;
+
+      await adminsRef.transaction(admins => {
+        if (admins) {
+          if (admins[uid]) {
+            admins[uid] = null;
+            alertMsg = 'Admin permission removed';
+          } else {
+            admins[uid] = true;
+            alertMsg = 'Admin permission granted';
+          }
+        }
+
+        return admins;
+      });
+
+      Alert.info(alertMsg, 4000);
+    },
+    [chatId]
+  );
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMessages &&
+        messages.map(msg => (
+          <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} />
+        ))}
+    </ul>
+  );
+};
+
+export default Messages;
