838a8989de156de5e04c1b0de0012404aeda26e1
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:41:38 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T14_File and audio chat messages/M4_T14_E4_Record and Upload Audio Messages - Part 4/src/misc/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+const config = {
+  apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
+  authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
+  projectId: 'chat-web-app-4ee4c',
+  storageBucket: 'chat-web-app-4ee4c.appspot.com',
+  messagingSenderId: '167319830934',
+  appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+};
+
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
+export const storage = app.storage();
