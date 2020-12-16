5669da2b8714fe60e8e83d81462fd7b52b159099
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:34:23 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T13_Messages likes and deletion/M4_T13_E1_Adding Programmatically Hover With Hooks/src/components/chat-window/messages/MessageItem.js
@@ -0,0 +1,64 @@
+import React, { memo } from 'react';
+import TimeAgo from 'timeago-react';
+import { Button } from 'rsuite';
+import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
+import { useHover } from '../../../misc/custom-hooks';
+
+const MessageItem = ({ message, handleAdmin }) => {
+  const { author, createdAt, text } = message;
+
+  const [selfRef, isHovered] = useHover();
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
+
+  return (
+    <li
+      className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`}
+      ref={selfRef}
+    >
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove admin permission'
+                : 'Give admin in this room'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
+      </div>
+
+      <div>
+        <span className="word-breal-all">{text}</span>
+      </div>
+    </li>
+  );
+};
+
+export default memo(MessageItem);
