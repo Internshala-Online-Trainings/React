16901916505f2c8301c7cb2d9eec0a711a302476
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:46:35 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T6_Creating and displaying chat rooms/M4_T6_E4_Creating Chat Rooms List - Show rooms and use them as links (Part 3)/src/misc/helpers.js
@@ -0,0 +1,17 @@
+export function getNameInitials(name) {
+  const splitName = name.toUpperCase().split(' ');
+
+  if (splitName.length > 1) {
+    return splitName[0][0] + splitName[1][0];
+  }
+
+  return splitName[0][0];
+}
+
+export function transformToArrWithId(snapVal) {
+  return snapVal
+    ? Object.keys(snapVal).map(roomId => {
+        return { ...snapVal[roomId], id: roomId };
+      })
+    : [];
+}
