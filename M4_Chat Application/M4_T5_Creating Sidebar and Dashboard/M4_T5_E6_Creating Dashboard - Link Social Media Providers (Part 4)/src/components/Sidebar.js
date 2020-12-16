c81ce88153942a9269f53aa9fd23c15642cf6e7b
Internshala-Online-Trainings <48007951+Internshala-Online-Trainings@users.noreply.github.com>
2020-12-16 14:44:46 +0530

    Add files via upload

---
+++ M4_Chat Application/M4_T5_Creating Sidebar and Dashboard/M4_T5_E6_Creating Dashboard - Link Social Media Providers (Part 4)/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashboardToggle />
+      </div>
+      bottom
+    </div>
+  );
+};
+
+export default Sidebar;
